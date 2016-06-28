bc7d50832e979a62d832c5cc31e8e0cef119e2fe
Simon Balderson <s.balderson@wells-cathedral-school.com>
2016-06-28 15:45:11 +0100

    basic web server

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log("Hello World!");
+var express = require('express');
+
+var app = express();
+var HTTP_PORT  = 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(HTTP_PORT);
+
+console.log('Webserver started on port ' + HTTP_PORT);
